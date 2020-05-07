f6c94eec9f5d1f28e147d6d5f1a502b8c2ac0234
alanricardos <alan.ricardo.silva@hotmail.com>
2020-05-07 11:14:53 -0300

    Vetores do tipo classe

---
+++ Vetores_2/Vetores_2/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores_2
+{
+    class Produto
+    {
+        public String Nome { get; set; }
+        public double Preco { get; set; }
+    
+    }
+
+}
---
+++ Vetores_2/Vetores_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Produto[] vect = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Produto { Nome = name, Preco = price };
+            }
+            double soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i].Preco;
+            }
+            double avg = soma / n;
+            Console.WriteLine("AVERAGE PRICE = "+ avg.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
