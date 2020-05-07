d3d5308ec15d8151f97d23e0af41f3a7191a88ef
alanricardos <alan.ricardo.silva@hotmail.com>
2020-05-07 14:03:00 -0300

    Lista Ligada

---
+++ Lista_ligada/Lista_ligada/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Lista_ligada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            List<string> list2 = new List<string> { "Maria", "Alex", "Bob" };
+
+            list.Add("Michael");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Ana");
+            list.Add("Maria");
+            list.Add("João");
+            list.Add("Matarazzo");
+            list.Add("Policarpo");
+            list.Insert(3, "Felipe");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("List count: " + list.Count);
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First A: " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last A: " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Position First A: " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last Position A: "+ pos2);
+
+            List<string> result = list.FindAll(x => x.Length >= 5);
+            Console.WriteLine("--------------------");
+            foreach (var item in result)
+            {
+                Console.WriteLine("["+item+"]");
+            }
+            list.Remove("Alex");
+            Console.WriteLine("--------------------");
+            foreach (var item in list)
+            {
+                Console.Write("[" + item + "]");
+            }
+            Console.WriteLine();
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("--------------------");
+            foreach (var item in list)
+            {
+                Console.Write("[" + item + "]");
+            }
+            list.RemoveAt(0);
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            foreach (var item in list)
+            {
+                Console.Write("[" + item + "]");
+            }
+            list.RemoveRange(0, 2);
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            foreach (var item in list)
+            {
+                Console.Write("[" + item + "]");
+            }
+
+
+        }
+
+    }
+}
--- Parametros/Parametros/Program.cs
+++ Parametros/Parametros/Program.cs
@@ -8,7 +8,7 @@ namespace Parametros
         {
             /*    int s1 = Calculadora.Sum(2, 3);
                 int s2 = Calculadora.Sum(2, 4, 3);
-
+                
                 Console.WriteLine(s1);
                 Console.WriteLine(s2);
             */
