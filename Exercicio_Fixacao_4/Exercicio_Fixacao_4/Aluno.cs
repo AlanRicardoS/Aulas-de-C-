1e22241ff53cf9ca95fcb4a8d93cb9f2d4c89020
alanricardos <alan.ricardo.silva@hotmail.com>
2020-05-07 11:57:00 -0300

    exercicio de fixação 4

---
+++ Exercicio_Fixacao_4/Exercicio_Fixacao_4/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Fixacao_4
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Aluno()
+        {
+           
+        }
+    }
+}
---
+++ Exercicio_Fixacao_4/Exercicio_Fixacao_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio_Fixacao_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Aluno[] vetor = new Aluno[9];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nome Aluno " + (i+1) + ": ");
+                string nome = Console.ReadLine();
+               
+                Console.Write("Email Aluno " + (i+1) + ": ");
+                string email = Console.ReadLine();
+              
+                Console.Write("Quarto Aluno " + (i+1) + ": ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[i] = new Aluno { Nome = nome, Email = email, Quarto = quarto };
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (vetor[i].Quarto != 0)
+                {
+                    Console.WriteLine(vetor[i].Quarto+": "+vetor[i].Nome+", "+ vetor[i].Email);
+                }
+                
+            }
+
+
+        }
+    }
+}
