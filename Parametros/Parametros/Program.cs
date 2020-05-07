79dace0f91240117d1785c1b4cb5dbb46661bedd
alanricardos <alan.ricardo.silva@hotmail.com>
2020-05-07 12:26:44 -0300

    Aula de Parametros e tipos de referencias

---
+++ Parametros/Parametros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*    int s1 = Calculadora.Sum(2, 3);
+                int s2 = Calculadora.Sum(2, 4, 3);
+
+                Console.WriteLine(s1);
+                Console.WriteLine(s2);
+            */
+            int a = 10;
+            int triple = 0;
+            Calculadora.Triple(a, out triple); // ref está passando o valor por referência
+            Console.WriteLine(triple);
+    }
+    }
+}
